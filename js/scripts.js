5fc12cb20aef74bdd691c6cb7b37752b6ac04246
Drew Yoxsimer and Sean Byram <sean@>
2019-01-14 16:36:56 -0800

    started cipher hw

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+
+
+function sentence(wrd) {
+  return wrd;
+};
+
+var words = prompt("Please type a sentence");
+
+console.log(sentence(words));
+
+
+
+
+ words.charAt(0);
+ words.charAt(words.length-1);
+
+console.log(firstChar);
+console.log(lastChar);
+
+
+console.log(firstChar.toUpperCase());
+console.log(lastChar.toUpperCase());
